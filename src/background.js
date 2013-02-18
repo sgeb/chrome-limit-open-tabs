c9060c6f11c80cde5b510f651a7d639d1178126d
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-02-17 23:00:07 -0300

    Simplify logic and fix count of pinned tabs

--- src/background.js
+++ src/background.js
@@ -2,38 +2,24 @@ chrome.runtime.onSuspend.addListener(function() {
     console.log("onSuspend");
 });
 
-function setTabsCount(value) {
-    localStorage.tabsCount = value;
-    console.log("tabsCount: " + value);
-    setBadge(value);
-}
-
-function getTabsCount() {
-    str_value = localStorage.tabsCount;
-    if (str_value == null || str_value == "null"){
-        value = 0;
-    }
-    else {
-        value = parseInt(str_value);
-    }
-    return value;
-}
-
-function addToTabsCount(value) {
-    setTabsCount(getTabsCount() + value);
-}
-
 function countTabs() {
     chrome.tabs.query({pinned: false}, function(tabs) {
-        setTabsCount(tabs.length);
+        console.log("tabsCount: " + tabs.length);
+        setBadge(tabs.length);
     });
 };
 
 function setBadge(value) {
     chrome.browserAction.setBadgeText({text: value.toString()});
-    
 }
 
+chrome.tabs.onCreated.addListener(countTabs);
+chrome.tabs.onRemoved.addListener(countTabs);
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo != null && changeInfo.pinned != null) {
+        countTabs();
+    }
+});
+
 countTabs();
-chrome.tabs.onCreated.addListener(function() {addToTabsCount(1)});
-chrome.tabs.onRemoved.addListener(function() {addToTabsCount(-1)});
